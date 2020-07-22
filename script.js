2b1238c995b41065785c4b85e4254912c632fc28
Tiffany Brand <brand2learn@gmail.com>
2020-07-21 20:35:47 -0400

    Add comments and delete console.logs

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
 $(document).ready(function () {
 
-    // DOM selectors
-
+    // Selector to display today's date
     const currentDateEl = $("header #currentDay");
 
-    const today = moment().subtract(5, "hours"); // when testing, change this to different hours - after testing, remove the subtract
+    // set today's date
+    const today = moment().subtract(4, "hours"); // when testing, change this to different hours - after testing, remove the subtract
 
+    // declare object to store calendar events
     let calEvents = {};
-    // let calEvents = {
-    //     "9AM": "Sleep In",
-    //     "10AM": "",
-    //     "11AM": "Program Coordinators Meeting",
-    //     "12PM": "Lunch",
-    //     "1PM": "Coding",
-    //     "2PM": "Pick up kids",
-    //     "3PM": "",
-    //     "4PM": "",
-    //     "5PM": ""
-    // }
 
-
-
-    // initialize calendar
-    function initCalendar() {
-        currentDateEl.text(today.format('LL'));
-        renderCalendar(today, calEvents);
-    }
-    loadCal();
-
-    initCalendar();
-
-    // build page
-
+    // render the calendar on the page
     function renderCalendar(date, calEvents) {
-
         date = moment(date).hour(9);
         const calendar = $("div.container");
         // loop to make blocks for hours 9 to 5
         for (let i = 1; i < 10; i++) {
             const row = $("<div>").addClass("row");
             let classOfHour = "";
-            // console.log(date.isBefore(today, "hour"))
-            // console.log(date.isSame(today, "hour"))
-            // console.log(date.isAfter(today, "hour"))
-            // console.log("date hour:" + date.hour())
-            // console.log("today hour:" + today.hour())
+
+            // set colors for time blocks
             if (today.isBefore(date, "hour")) {
                 classOfHour = "future"
             } else if (today.isAfter(date, "hour")) {
@@ -52,27 +26,29 @@ $(document).ready(function () {
             } else {
                 classOfHour = "present"
             }
-            console.log(classOfHour)
+
             calendar.append(row);
-
+            // hour column
             row.append($("<div>").addClass("col-sm-2 hour").text(date.format("h A")))
-            // create description div and color for past, present, or future hour
-            let timeBlock = date.format("hA");
-            console.log(timeBlock);
-
+            // event description column
+            let timeBlock = date.format("hA"); // keys for data in calEvents object to populate textarea
             row.append($("<textarea>").addClass(`col-sm-8 ${classOfHour}`).text(calEvents[timeBlock]));
-
+            // save button column
             row.append($("<button>").addClass("col-sm-2 saveBtn").html("<i class='fas fa-save'></i>").attr("id", date.format("hA")));
+
+            // increment hour before creating next row
             date.add(1, "hour");
         }
     }
 
 
+    // initialize calendar
+    function initCalendar() {
+        currentDateEl.text(today.format('LL'));
+        renderCalendar(today, calEvents);
+    }
 
-    function storeCal() {
-        localStorage.setItem("calEvents", JSON.stringify(calEvents));
-    };
-
+    // loads events from local storage
     function loadCal() {
         const storedCal = JSON.parse(localStorage.getItem("calEvents"));
         if (storedCal) {
@@ -80,32 +56,25 @@ $(document).ready(function () {
         }
     }
 
+
+    // load calendar events from local storage and render the calendar for today
+    loadCal();
+    initCalendar();
+
+
+    // store calendar events in local storage
+    function storeCal() {
+        localStorage.setItem("calEvents", JSON.stringify(calEvents));
+    };
+
+
+
     // save calendar event
     $("button.saveBtn").on("click", function (event) {
-        console.log(event.currentTarget)
-        console.log(event.currentTarget.id);
         let calDesc = event.currentTarget.parentElement.children[1].value;
-        console.log(calDesc);
         calEvents[event.currentTarget.id] = calDesc;
-        console.log(calEvents);
         storeCal();
-
-
     })
-
-
-
-
-    // trying stuff out
-
-    console.log(today.format("h A"))
-    console.log(today.hour(9).format("h A"));
-    console.log(today.add(1, "hour").format("h A"))
-
-
-
-
-
 
 
 
